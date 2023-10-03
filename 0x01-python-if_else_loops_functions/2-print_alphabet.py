453297a5e92c26a3a096be3eb261aa75f7c67ea9
AbdelRahman-AS <121566682+AbdelRahman-AS@users.noreply.github.com>
2023-10-03 19:53:38 +0000

    till 4

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for c in range(97, 123):
-    print("{}".format(chr(c)), end="")
+for ci in range(97, 123):
+    print("{}".format(chr(ci)), end="")
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+for ci in range(97, 123):
+    if chr(ci) != 'q' and chr(ci) != 'e':
+        print("{}".format(chr(ci)), end="")
