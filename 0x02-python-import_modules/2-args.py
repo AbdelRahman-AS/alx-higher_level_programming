5480ccd9a2b01197a30788545dd64888a07fde6a
AbdelRahman-as <abdelrahmansam@gmail.com>
2023-10-05 10:26:05 +0300

    2 3

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ != "__main__":
+	exit
+import sys
+count = len(sys.argv) - 1
+if count == 0:
+	print(f"{count} arguments.")
+elif count == 1:
+	print("1 argument:")
+else:
+	print(f"{count} arguments:")
+
+for i in range(0,count):
+	print(f"{i +1}: {sys.argv[i+1]} ")
+
