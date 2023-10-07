35c4e01565c297fef9f08d3fde3b10429359057e
AbdelRahman-as <abdelrahmansam@gmail.com>
2023-10-07 17:21:47 +0300

    fixxx

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,11 +2,7 @@
 def element_at(my_list, idx):
     length = len(my_list)
     if idx > length or idx < 0:
-<<<<<<< HEAD
-        return "None"
-=======
         return None
->>>>>>> d39fd9f82871bfe60d2c0f211f2eb231cfa25eb4
     for i in range(0, length):
         if i == idx:
             return my_list[i]
